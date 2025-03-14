65c7cc98b7ae9206c6df805e3577872bf989716e
dleonaraujo <114316707+dleonaraujo@users.noreply.github.com>
2025-03-14 12:07:45 -0500

    Integrando manejo de random para la eleccion de la bot

--- unidad I/practica_1/src/ejercicio2/MainEjercicio2.kt
+++ unidad I/practica_1/src/ejercicio2/MainEjercicio2.kt
@@ -8,4 +8,17 @@ fun main (){
     }
     
     print("Su opcion: ")
+
+    val jugador = readLine()?.toIntOrNull()
+
+    if (jugador == null || jugador !in 0..2) {
+        println("¡Opción no válida!")
+        return
+    }
+
+    val opcionJugador = opciones[jugador]
+    val opcionBot = opciones[Random.nextInt(3)]
+
+    println("Usted eligió: $opcionJugador")
+    println("La computadora eligió: $opcionBot")
 }
