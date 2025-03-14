c5ae9cfe4136996bed9bbc58a9f752436dd5f1d2
dleonaraujo <114316707+dleonaraujo@users.noreply.github.com>
2025-03-14 12:04:50 -0500

    Integrando for each para el muestreo de las opciones

--- unidad I/practica_1/src/ejercicio2/MainEjercicio2.kt
+++ unidad I/practica_1/src/ejercicio2/MainEjercicio2.kt
@@ -1,1 +1,11 @@
-
+fun main (){
+    val opciones = listOf("piedra", "papel", "tijera")
+    
+    println("Elija una Opcion")
+    
+    opciones.forEachIndexed { index, opcion ->
+        println("$index: $opcion")
+    }
+    
+    print("Su opcion: ")
+}
