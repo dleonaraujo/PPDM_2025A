822d371896887a8dfab6ac767ab202baaeacd9a4
Diego Leon Araujo <dleona@ulasalle.edu.pe>
2025-03-21 11:23:38 -0500

    Estableciendo un metodo setter para incorporar un nuevo precio al producto

--- unidad I/practica_2/src/ejercicio_2/MainEjercicio2.kt
+++ unidad I/practica_2/src/ejercicio_2/MainEjercicio2.kt
@@ -7,4 +7,13 @@ class Producto(private var precio: Double, private var descuento: Double) {
         return precio
     }
 
+    // Setter para establecer el precio con validación
+    fun setPrecio(nuevoPrecio: Double) {
+        if (nuevoPrecio >= 0) {
+            precio = nuevoPrecio
+        } else {
+            println("Error: El precio no puede ser negativo.")
+        }
+    }
+    
 }
