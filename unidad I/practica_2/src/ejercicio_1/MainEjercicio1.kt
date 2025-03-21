58e7654c639df08c21fed4f47086548637fae2ec
Diego Leon Araujo <dleona@ulasalle.edu.pe>
2025-03-21 11:15:57 -0500

    Integrando el metodo set para establecer el saldo con validacion

--- unidad I/practica_2/src/ejercicio_1/MainEjercicio1.kt
+++ unidad I/practica_2/src/ejercicio_1/MainEjercicio1.kt
@@ -8,5 +8,14 @@ class CuentaBancaria(private var saldo: Double, private var limiteRetiro: Double
         return saldo
     }
 
+    // Setter para establecer el saldo con validación
+    fun setSaldo(nuevoSaldo: Double) {
+        if (nuevoSaldo >= 0) {
+            saldo = nuevoSaldo
+        } else {
+            println("Error: El saldo no puede ser negativo.")
+        }
+    }
+
 
 }
