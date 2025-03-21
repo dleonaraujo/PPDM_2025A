c27432780f860938a270cd2f9f4cf089c128538e
Diego Leon Araujo <dleona@ulasalle.edu.pe>
2025-03-21 11:14:58 -0500

    Creando clase CuentaBancaria, e integrando el metodo get para obtener el saldo

--- unidad I/practica_2/src/ejercicio_1/MainEjercicio1.kt
+++ unidad I/practica_2/src/ejercicio_1/MainEjercicio1.kt
@@ -1,1 +1,12 @@
 package ejercicio_1
+
+
+class CuentaBancaria(private var saldo: Double, private var limiteRetiro: Double) {
+
+    // Getter para obtener el saldo
+    fun getSaldo(): Double {
+        return saldo
+    }
+
+
+}
