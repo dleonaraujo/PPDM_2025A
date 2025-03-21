ad447aa97b284b9927e9a2949bb90eca76afb16c
Diego Leon Araujo <dleona@ulasalle.edu.pe>
2025-03-21 11:22:36 -0500

    Integrando la clase Producto, con el metodo getPrecio, para obtener el precio del producto

--- unidad I/practica_2/src/ejercicio_2/MainEjercicio2.kt
+++ unidad I/practica_2/src/ejercicio_2/MainEjercicio2.kt
@@ -1,4 +1,10 @@
 package ejercicio_2
 
-class MainEjercicio2 {
+class Producto(private var precio: Double, private var descuento: Double) {
+
+    // Getter para obtener el precio
+    fun getPrecio(): Double {
+        return precio
+    }
+
 }
