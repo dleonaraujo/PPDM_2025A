912253c7af824f10a68551fb2b797c4dc0c01912
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-03-29 12:31:20 -0500

    Integrando imagen solicitada, ademas de integrar una vista en xml, la cual es llamada por activity main

--- unidadI/practica_3/app/src/main/java/com/ppdm/appimagenes/MainActivity.kt
+++ unidadI/practica_3/app/src/main/java/com/ppdm/appimagenes/MainActivity.kt
@@ -16,32 +16,6 @@ import com.ppdm.appimagenes.ui.theme.AppImagenesTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AppImagenesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppImagenesTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
