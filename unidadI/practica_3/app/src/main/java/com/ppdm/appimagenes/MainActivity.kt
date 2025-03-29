440281e3254bcd3e11439b44f6f0542ea92660a2
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-03-29 12:35:28 -0500

    Retirando librerias innecesarias

--- unidadI/practica_3/app/src/main/java/com/ppdm/appimagenes/MainActivity.kt
+++ unidadI/practica_3/app/src/main/java/com/ppdm/appimagenes/MainActivity.kt
@@ -4,16 +4,6 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ppdm.appimagenes.ui.theme.AppImagenesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
