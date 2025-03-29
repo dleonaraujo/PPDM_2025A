c1b215f5017dae7d7fa69abceed8aeca3bad34b5
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-03-29 12:34:44 -0500

    Integrando lógica del Toast para mostrar un mensaje al tocar la imagen

--- unidadI/practica_3/app/src/main/java/com/ppdm/appimagenes/MainActivity.kt
+++ unidadI/practica_3/app/src/main/java/com/ppdm/appimagenes/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ppdm.appimagenes
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,5 +19,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView: ImageView = findViewById(R.id.imageView)
+
+        imageView.setOnClickListener {
+            Toast.makeText(this, "Bienvenido a la Torre Eiffel!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
