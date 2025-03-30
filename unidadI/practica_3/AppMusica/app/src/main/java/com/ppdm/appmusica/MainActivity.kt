3e5112fe5577e00db55675ac18e1e76feb7c439e
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-03-30 11:11:23 -0500

    Integrando logica para el reproductor de musica, con sus botones de play, pausa, detener

--- unidadI/practica_3/AppMusica/app/src/main/java/com/ppdm/appmusica/MainActivity.kt
+++ unidadI/practica_3/AppMusica/app/src/main/java/com/ppdm/appmusica/MainActivity.kt
@@ -18,5 +18,34 @@ class MainActivity : AppCompatActivity() {
         val botonParar = findViewById<Button>(R.id.botonParar)
 
         mediaPlayer = MediaPlayer.create(this, R.raw.guns_sweet_child_o_mine)
+
+        botonReproducir.setOnClickListener {
+            if (!isPlaying) {
+                mediaPlayer.start()
+                isPlaying = true
+            }
+        }
+
+        botonPausa.setOnClickListener {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.pause()
+                isPlaying = false
+            }
+        }
+
+        botonParar.setOnClickListener {
+            if (isPlaying) {
+                mediaPlayer.stop()
+                mediaPlayer.release()  // Libera el recurso
+                mediaPlayer = MediaPlayer.create(this, R.raw.guns_sweet_child_o_mine)  // Crea uno nuevo
+                isPlaying = false
+            }
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release() // Liberar recursos cuando la app se cierre
     }
 }
