d7f1f7c4b93a7af0f30aedd3518f0dfee5d324d4
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-04-14 11:34:45 -0500

    Integrando .gitignore y base del proyecto para la app de edicion de perfikl

---
+++ unidadII/practica_4/app_edicion_perfil/app/src/main/java/com/ppdm/app_edicion_perfil/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ppdm.app_edicion_perfil
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ppdm.app_edicion_perfil.ui.theme.App_edicion_perfilTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            App_edicion_perfilTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    App_edicion_perfilTheme {
+        Greeting("Android")
+    }
+}
