92bf204bda7ccce7c05e9c7560a64f095e585bde
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-04-14 11:37:07 -0500

    Limpiando MainActivity

--- unidadII/practica_4/app_edicion_perfil/app/src/main/java/com/ppdm/app_edicion_perfil/MainActivity.kt
+++ unidadII/practica_4/app_edicion_perfil/app/src/main/java/com/ppdm/app_edicion_perfil/MainActivity.kt
@@ -16,32 +16,5 @@ import com.ppdm.app_edicion_perfil.ui.theme.App_edicion_perfilTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            App_edicion_perfilTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    App_edicion_perfilTheme {
-        Greeting("Android")
     }
 }
