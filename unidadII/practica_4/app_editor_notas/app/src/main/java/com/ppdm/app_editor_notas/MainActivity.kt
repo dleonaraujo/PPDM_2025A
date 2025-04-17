9d0c7994fa3d063ec1daf3951916997b03ec1462
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-04-17 11:30:10 -0500

    Integrando Lògica en la clase MainActivity, con respecto a layout activity_main.xml, para el manejo de la escritura

--- unidadII/practica_4/app_editor_notas/app/src/main/java/com/ppdm/app_editor_notas/MainActivity.kt
+++ unidadII/practica_4/app_editor_notas/app/src/main/java/com/ppdm/app_editor_notas/MainActivity.kt
@@ -1,20 +1,41 @@
-package com.ppdm.app_editor_notas
+package com.ppdm.editornotasapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ppdm.app_editor_notas.ui.theme.App_editor_notasTheme
+import android.widget.Button
+import android.widget.EditText
+import com.ppdm.app_editor_notas.R
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
+    private lateinit var notaEditText: EditText
+    private val REQUEST_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        notaEditText = findViewById(R.id.notaEditText)
+        val btnCompartir = findViewById<Button>(R.id.btnCompartir)
+        
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            val nuevaNota = data?.getStringExtra("nota")
+            notaEditText.setText(nuevaNota)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("nota_guardada", notaEditText.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        notaEditText.setText(savedInstanceState.getString("nota_guardada", ""))
     }
 }
