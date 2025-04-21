e7e5dfa127cf70262f672aa2f0af39368d4de611
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-04-21 12:48:06 -0500

    subiendo toda la logica entre fragments

--- unidadII/app_pedidos/app/src/main/java/com/ppdm/app_pedidos/InicioFragment.kt
+++ unidadII/app_pedidos/app/src/main/java/com/ppdm/app_pedidos/InicioFragment.kt
@@ -6,13 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.ppdm.app_pedidos.databinding.FragmentInicioBinding
 
 class InicioFragment : Fragment() {
 
+    private lateinit var binding: FragmentInicioBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        binding = FragmentInicioBinding.inflate(inflater, container, false)
+
+        binding.btnNuevoPedido.setOnClickListener {
+            findNavController().navigate(R.id.action_inicioFragment_to_seleccionComidaFragment)
+        }
+
+        return binding.root
     }
 }
+
